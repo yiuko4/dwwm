231be97aebe398b150d8f0558511ff1884fc4eeb
yiuko4 <yiuko4@icloud.com>
2023-11-14 19:02:50 +0100

    d

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+/*aeaeaeaz*/
 define("URL", str_replace("index.php", "", (isset($_SERVER['HTTPS']) ? "https" : "http") .
     "://" . $_SERVER['HTTP_HOST'] . $_SERVER["PHP_SELF"]));
 
